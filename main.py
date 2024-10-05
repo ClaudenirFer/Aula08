32f871a497c0c25f474789c249736b5bd10e5bf3
ClaudenirFer <claudenir_fer@yahoo.com.br>
2024-10-05 00:02:08 -0300

    Implementa a função somar

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+def somar(a, b):
+    s = a + b
+    return s
+
+
 def saudacao():
-  print('Olá Mundo')
+    print('Olá Mundo')
+
 
 # início do programa
 print('Menu inicial')
+x = 5
+y = 3
+
+total = somar(x, y)
